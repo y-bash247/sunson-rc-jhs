855771255c88e21ceecad1007fa1041e05fbc145
y-bash247 <yakububashiru832@gmail.com>
2026-08-29 20:58:18 +0000

    Delete sunson-rc- jhs directory

--- sunson-rc- jhs/popup.js
+++
@@ -1,6 +0,0 @@
-const openBtn = document.getElementById('openBtn');
-const closeBtn = document.getElementById('closeBtn');
-const overlay = document.getElementById('popupOverlay');
-
-openBtn.addEventListener('click', () => overlay.classList.add('open'));
-closeBtn.addEventListener('click', () => overlay.classList.remove('open'));
